d74a9961e35a77ec7623cc8514d2b14841c545f4
Zach <zach.bay@gmail.com>
2016-05-24 19:33:41 -0700

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">Hi, I'm an about page.</p>
     </div>
   )
 };
